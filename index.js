1db55a9890215474efcba48e56f19f5299f32d1d
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2017-07-10 11:25:59 +0200

    process cli args

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const Kitsu = require('kitsu');
 const https = require('https');
 const Q = require('q');
+const fs = require('fs');
 
 const kitsu = new Kitsu();
 
-downloadAnime(20, 0).then(a => {
+let offset = process.argv[2];
+let filePrefix = process.argv[3];
+let outputFolder = process.argv[4];
+
+if (!offset || !filePrefix || !outputFolder) {
+  console.error(`Incorrect usage. Use 'node index.js <offset> <filePrefix> <outputFolder>'.`);
+  process.exit(1);
+}
+
+downloadAnime(20, offset).then(a => {
   delete a.links;
-  console.log(a);
+  let filename = `${filePrefix}-${offset}.json`;
+  let path = `${outputFolder}/${filename}`;
+
+  fs.writeFile(path, JSON.stringify(a), err => {
+    if (err) {
+      console.error(`Something went wrong! We couldn't write the content to file '${path}'.`);
+      console.error(err);
+    }
+  });
 });
 
 function downloadAnime(limit, offset) {
